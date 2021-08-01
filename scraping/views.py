47484ac2ee4842bd7a7b7c1820f6e43d29a3dfa3
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-08-01 13:25:39 +0200

    scrate village name and get data from search

--- scraping/views.py
+++ scraping/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+import requests
+from bs4 import BeautifulSoup
 
 
 def home(request):
+    if request.method == "POST":
+        psc = request.POST.get("psc")
+        distance = request.POST.get("select_field")
+        choice = request.POST.get("group1")
+
+        if choice == "land":
+            choice = "Pozemky"
+        elif choice == "house":
+            choice = "Domy"
+        else:
+            choice = "Byty"
+
+        psc_url = f"https://www.psc.cz/{psc}/"
+        session = requests.Session().get(psc_url).text
+        html = BeautifulSoup(session, "html.parser")
+        village_name = html.find(
+            "div", class_="psc-text").h1.text.split(",")[1]
+        
+
+        return render(request, "home.html", {"village_name": village_name, "distance": distance, "choice": choice})
+
     return render(request, "home.html")
