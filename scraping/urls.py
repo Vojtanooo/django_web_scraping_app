a2cddf3959c711f32cc99d16f06fc3f0558bd7e8
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-08-11 14:00:00 +0200

    create style.css file and favicon

--- scraping/urls.py
+++ scraping/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('search/', views.home, name="home"),
+    path('', views.delete_session, name="delete-session")
 ]
